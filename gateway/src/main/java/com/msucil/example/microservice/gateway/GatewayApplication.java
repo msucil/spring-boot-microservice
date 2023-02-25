75b5f2a74fdac4a92e38ebb407f8c167824902fc
msucil <np.msushil@gmail.com>
2023-02-25 12:06:56 +0545

    Register Gateway to discovery client

--- gateway/src/main/java/com/msucil/example/microservice/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/msucil/example/microservice/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.msucil.example.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
