af8ea45abdaa007bb4ecb69ea54b7031a6e07c08
Sushil Ale <msucil@Sushils-MacBook-Pro.local>
2023-02-25 11:10:43 +0545

    initialse microservice example repo

---
+++ gateway/src/main/java/com/msucil/example/microservice/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msucil.example.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ info/src/test/java/com/msucil/example/microservice/info/InfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msucil.example.microservice.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
