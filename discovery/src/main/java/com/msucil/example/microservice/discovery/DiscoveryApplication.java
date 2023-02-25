820d57954cceb167efa2f0ee45e2415d79c64c00
msucil <np.msushil@gmail.com>
2023-02-25 11:39:23 +0545

    Add DiscoveryServer configuration

--- discovery/src/main/java/com/msucil/example/microservice/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/msucil/example/microservice/discovery/DiscoveryApplication.java
@@ -1,13 +1,16 @@
 package com.msucil.example.microservice.discovery;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
+		new SpringApplicationBuilder(DiscoveryApplication.class).web(WebApplicationType.SERVLET).run(args);
 	}
 
 }
